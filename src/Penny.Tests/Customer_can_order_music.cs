72cbc9bb09bc85286121a9507782dd32859548ff
Leon <leon.hewitt@7digital.com>
2013-01-24 15:57:26 +0000

    First two tests

---
+++ src/Penny.Tests/Customer_can_order_music.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Penny.Tests
+{
+	[TestFixture]
+    public class Customer_can_order_music
+    {
+		private string _result;
+		private bool _cardCharged;
+		private const string _ok = "OK";
+
+		[SetUp]
+		public void SetUp()
+		{
+			_cardCharged = false;
+			_result = SubmitOrder(ref _cardCharged);
+		}
+
+		[Test]
+		public void Should_get_an_ok_when_submitting_an_order()
+		{
+			Assert.That(_result,Is.EqualTo(_ok));
+		}
+
+		[Test]
+		public void Should_charge_my_card()
+		{
+			Assert.IsTrue(_cardCharged);
+		}
+
+		private string SubmitOrder(ref bool cardCharged)
+		{
+			cardCharged = true;
+			return _ok;
+		}
+    }
+}
